732a1c456cdf17829581e8077b1aa95426f39e92
ghofrane-dridi <37979361+ghofrane-dridi@users.noreply.github.com>
2025-07-21 13:55:29 +0200

    Update User.java

---
+++ src/main/java/com/example/devsecops/model/User.java
@@ -0,0 +1,15 @@
+package com.example.devsecops.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    // Getters et setters
+}
